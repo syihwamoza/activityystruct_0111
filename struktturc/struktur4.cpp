414008bbe43773c41ce9b23876c2460355c823c3
syihwamoza <mozakastella26@gmail.com>
2024-04-01 20:24:07 +0700

    memasukkan for kedua untuk menmpilkab

--- struktturc/struktur4.cpp
+++ struktturc/struktur4.cpp
@@ -30,3 +30,15 @@ int main() {
             cin.ignore(1, '\n');
             cout << endl;
         }
+        for (int i = 0; i < 3; i++) {
+            cout << endl;
+            cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+            cout << "\n NIM : " << mhs[i].nim;
+            cout << "\n Nama : " << mhs[i].nama;
+            cout << "\n Alamat : ";
+            cout << "\n \t Desa : " << mhs[i].alamat.desa;
+            cout << "\n \t Kota : " << mhs[i].alamat.kota;
+            cout << "\n Umur : " << mhs[i].umur;
+            cout << endl;
+        }
+}
