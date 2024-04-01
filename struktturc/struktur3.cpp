860ce426053afc24c2a626aaa608881efa4a1452
syihwamoza <mozakastella26@gmail.com>
2024-04-01 19:58:49 +0700

    memasukkan for dalam prosedur main

--- struktturc/struktur3.cpp
+++ struktturc/struktur3.cpp
@@ -13,6 +13,21 @@ struct Mahasiswa {
 };
 int main() {
 	Mahasiswa mhs[3];
-	
+	for (int i = 0; 1 < 3; i++) {
+		cout << "Data ke- " << (i+1) << ":" << endl;
+		cout << "Nomor Mahasiswa :";
+		getline ( cin, mhs[i].nim);
+		cout << "Nama Mahasiswa :";
+		getline(cin, mhs[i].nama);
+
+		cout << "Alamat Mahasiswa :" << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t nama kota :";
+		cin >> mhs[i].alamat.kota;
+		cout << "umur mahasiswa :";
+		cin >> mhs[i].alamat.umur;
+		cin.ignore(1, '\n');
+		cout << endl;
 	}
 }
