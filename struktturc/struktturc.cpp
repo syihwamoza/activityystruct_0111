0b784885ab64ce066b9596ac54e10aa1518f171c
syihwamoza <mozakastella26@gmail.com>
2024-04-01 19:13:07 +0700

    prosedur untuk menapilan data

--- struktturc/struktturc.cpp
+++ struktturc/struktturc.cpp
@@ -17,4 +17,11 @@ int main() {
 	cin >> mhs.alamat;
 	cout << "umur mahasiswa :";
 	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat :" << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+
 }
